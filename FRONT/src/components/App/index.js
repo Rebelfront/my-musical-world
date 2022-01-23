b87d97deea0a393ea1bccce52922a8df376ff847
AnthonyDurand23 <durand.anthony23@gmail.com>
2022-01-23 18:03:19 +0100

    feat-legalPage: page without CSS done

--- FRONT/src/components/App/index.js
+++ FRONT/src/components/App/index.js
@@ -1,17 +1,11 @@
-// == Import
-import reactLogo from './react-logo.svg';
-import './styles.css';
+import './style.scss';
 
-import Counter from '../Counter';
+import Legal from 'src/components/Legal';
 
-// == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
-    <Counter />
+    <Legal />
   </div>
 );
 
-// == Export
 export default App;
