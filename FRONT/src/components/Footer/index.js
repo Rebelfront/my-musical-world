1004c8c998b9c763617593410835776c28e131fc
AnthonyDurand23 <durand.anthony23@gmail.com>
2022-01-25 10:43:51 +0100

    feat-footer: footer without css done

---
+++ FRONT/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import './style.scss';
+
+const Footer = () => (
+  <div className="footer">
+    <a href="">A propos</a>
+    <p>2022 | My Musical World</p>
+    <a href="">Mentions légales</a>
+  </div>
+);
+
+export default Footer;
