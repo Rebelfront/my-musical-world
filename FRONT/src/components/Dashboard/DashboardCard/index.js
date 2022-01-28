ec2c717b38ef158deca6f9f3b385e8504420d6e3
arnaud rebel <arnaud.rebel@gmail.com>
2022-01-28 18:26:56 +0100

    feat-dashboard: dahsboard desktop wip

--- FRONT/src/components/Dashboard/DashboardCard/index.js
+++ FRONT/src/components/Dashboard/DashboardCard/index.js
@@ -1,32 +1,44 @@
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const DashboardCard = () => {
+const DashboardCard = ({
+  name,
+  genre,
+  artist,
+  year,
+  urlImage,
+  type,
+  album
+}) => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 200, mb: '20px' }}>
       <CardMedia
         component="img"
-        alt="green iguana"
-        height="140"
-        image="/static/images/cards/contemplative-reptile.jpg"
+        alt={name}
+        image={urlImage}
       />
       <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
+        {type !== "artist" && (
+          <Typography gutterBottom variant="h5" component="div">
+            {artist}
+          </Typography>
+        )}
+        <Typography sx={type !== "artist" ? { fontStyle: 'italic' } : {}} gutterBottom variant="h5" component="div">
+          {type === "artist" ? name :`"${name}"`} {year && type === "album" && `(${year})`}
         </Typography>
-        <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
+        {type === "track" && (
+          <Typography variant="body2" color="text.secondary">
+            Album: {`"${album}" (${year})`}
+          </Typography>
+        )}
+        {genre && (
+          <Typography variant="body2" color="text.secondary">
+            Genre: {genre}
+          </Typography>
+        )}
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 };
--- FRONT/src/components/Dashboard/index.js
+++ FRONT/src/components/Dashboard/index.js
@@ -4,10 +4,18 @@ import MusicNoteIcon from '@mui/icons-material/MusicNote';
 
 import './style.scss';
 
+import datas from 'src/datas/dashboard';
+import { Box } from "@mui/material";
+
 const Dashboard = () => {
+
+
+  console.log(datas);
+
   return(
     <div className="dashboard">
       <aside className="dashboard__menu">
+        <h2 className="dashboard__title">Mon dashboard</h2>
         <button className="dashboard__add-btn" type="button">
           <span>+</span>
           <MusicNoteIcon sx={{ color: '#ffffff' }} fontSize="large" />
@@ -15,10 +23,24 @@ const Dashboard = () => {
       </aside>
       <main className="dashboard__content">
         <Container maxWidth="md">
-          <DashboardCard />
-          <DashboardCard />
-          <DashboardCard />
-          <DashboardCard />
+          <Box sx={{ mb: '10px'}}>
+            <h3>Artistes</h3>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+              {datas.artists.map((artist) => <DashboardCard type="artist" key={artist.apiId} {...artist} />)}
+            </Box>
+          </Box>
+          <Box sx={{ mb: '10px'}}>
+            <h3>Albums</h3>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+              {datas.albums.map((album) => <DashboardCard type="album" key={album.apiId} {...album} />)}
+            </Box>
+          </Box>
+          <Box>
+            <h3>Titres</h3>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+              {datas.tracks.map((track) => <DashboardCard type="track" key={track.apiId} {...track} />)}
+            </Box>
+          </Box>
         </Container>
       </main>
     </ div>
