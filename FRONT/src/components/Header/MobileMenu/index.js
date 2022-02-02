bbe67e7229decbd16bd78dbc7903d9168520217b
arnaud rebel <arnaud.rebel@gmail.com>
2022-02-02 15:38:36 +0100

    feat-style: mobile menu style done

--- FRONT/src/components/Header/MobileMenu/index.js
+++ FRONT/src/components/Header/MobileMenu/index.js
@@ -15,11 +15,13 @@ import ListItemText from '@mui/material/ListItemText';
 
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
-import {
-  Accordion, AccordionDetails, AccordionSummary, IconButton, Typography,
-} from '@mui/material';
+import { IconButton } from '@mui/material';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AlbumIcon from '@mui/icons-material/Album';
+import InfoIcon from '@mui/icons-material/Info';
+import CopyrightIcon from '@mui/icons-material/Copyright';
 
 import './style.scss';
 
@@ -71,36 +73,61 @@ const MobileMenu = () => {
           </ListItem>
         </List>
       ) : (
-        <List>
-          <ListItem>
-            <Accordion sx={{ display: 'block', width: '100%' }}>
-              <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-              >
-                <Typography>
-                  <AccountCircleIcon className="align-middle" sx={{ mr: '10px' }} />
-                  <span className="align-middle">
-                    {pseudo.toUpperCase()}
-                  </span>
-                </Typography>
-              </AccordionSummary>
-              <AccordionDetails sx={{ display: 'flex', flexDirection: 'column' }}>
-                <Button className="button-green" sx={{ display: 'block', mb: '10px' }} onClick={handleOpenProfileModal}>
-                  <ListItemText primary="Mon profil" />
-                </Button>
-                <NavLink to="/dashboard" onClick={handleMenuToggle}>Ma bibliothèque</NavLink>
-                <Button sx={{ display: 'block', mt: '20px' }} className="button-red" onClick={handleLogout}>
-                  <ListItemText primary="Se déconnecter" />
-                </Button>
-              </AccordionDetails>
-            </Accordion>
-          </ListItem>
-        </List>
+        // <List>
+        //   <ListItem>
+        //     <Accordion sx={{ display: 'block', width: '100%' }}>
+        //       <AccordionSummary
+        //         expandIcon={<ExpandMoreIcon />}
+        //         aria-controls="panel1a-content"
+        //         id="panel1a-header"
+        //       >
+        //         <Typography>
+        //           <AccountCircleIcon className="align-middle" sx={{ mr: '10px' }} />
+        //           <span className="align-middle">
+        //             {pseudo.toUpperCase()}
+        //           </span>
+        //         </Typography>
+        //       </AccordionSummary>
+        //       <AccordionDetails sx={{ display: 'flex', flexDirection: 'column' }}>
+        //         <Button className="button-green" sx={{ display: 'block', mb: '10px' }} onClick={handleOpenProfileModal}>
+        //           <ListItemText primary="Mon profil" />
+        //         </Button>
+        //         <NavLink to="/dashboard" onClick={handleMenuToggle}>Ma bibliothèque</NavLink>
+        //         <Button sx={{ display: 'block', mt: '20px' }} className="button-red" onClick={handleLogout}>
+        //           <ListItemText primary="Se déconnecter" />
+        //         </Button>
+        //       </AccordionDetails>
+        //     </Accordion>
+        //   </ListItem>
+        // </List>
+        <>
+          <button className="mobile-menu__link" onClick={handleOpenProfileModal} type="button">
+            <AccountCircleIcon sx={{ mr: '10px' }} />
+            <span>Mon profil</span>
+            <ArrowForwardIosIcon sx={{ ml: 'auto' }} fontSize="small" />
+          </button>
+          <NavLink className="mobile-menu__link" to="/dashboard" onClick={handleMenuToggle}>
+            <AlbumIcon sx={{ mr: '10px' }} />
+            <span>Ma bibliothèque</span>
+            <ArrowForwardIosIcon sx={{ ml: 'auto' }} fontSize="small" />
+          </NavLink>
+          <button className="mobile-menu__link" onClick={handleLogout} type="button">
+            <LogoutIcon sx={{ mr: '10px' }} fontSize="medium" />
+            <span>Se déconnecter</span>
+            <ArrowForwardIosIcon sx={{ ml: 'auto' }} fontSize="small" />
+          </button>
+        </>
       )}
-      <NavLink className="mobile-menu__link" to="/about" onClick={handleMenuToggle}>A propos &gt;</NavLink>
-      <NavLink className="mobile-menu__link" to="/legal" onClick={handleMenuToggle}>Mentions légales &gt;</NavLink>
+      <NavLink className="mobile-menu__link" to="/about" onClick={handleMenuToggle}>
+        <InfoIcon sx={{ mr: '10px' }} fontSize="medium" />
+        <span>A propos</span>
+        <ArrowForwardIosIcon sx={{ ml: 'auto' }} fontSize="small" />
+      </NavLink>
+      <NavLink className="mobile-menu__link" to="/legal" onClick={handleMenuToggle}>
+        <CopyrightIcon sx={{ mr: '10px' }} fontSize="medium" />
+        <span>Mentions légales </span>
+        <ArrowForwardIosIcon sx={{ ml: 'auto' }} fontSize="small" />
+      </NavLink>
     </Box>
   );
 
