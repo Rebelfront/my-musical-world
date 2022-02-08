9dacad25b37ccbd4f4268cc9302b39250f125ec4
Etudiant O'clock <claire_1105@hotmail.fr>
2022-02-08 11:08:27 +0100

    tests

--- BACK/app/models/music.js
+++ BACK/app/models/music.js
@@ -2,22 +2,12 @@ const client = require('../database');
 
 class Music {
 
-<<<<<<< HEAD
-    static async getMusic(pseudo) {
-        try {
-
-            const music = await client.query('SELECT * FROM user_music WHERE pseudo=$1', [pseudo]);
-
-=======
 
     static async getMusic(pseudo) {
         try {
 
             const music = await client.query('SELECT * FROM user_music WHERE pseudo=$1', [pseudo]);
 
-
-
->>>>>>> develop
             if (music.rows[0].artists[0] === null) {
                 music.rows[0].artists = [];
             }
@@ -40,14 +30,6 @@ class Music {
         }
     }
 
-
-
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> develop
 };
 
 
